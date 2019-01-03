f7e5a5a43589c87f21d8df337e371d1df349e955
Dusto´n Lomrt <simon.m.f.w.knott@eclipso.de>
2019-01-03 21:15:05 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,9 @@
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 
-
+end
+def position_taken?(board, index)
+  board[index] != " "
 end# code your #valid_move? method here
 
 
